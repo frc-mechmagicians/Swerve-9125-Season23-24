236b200841cc59427fae7dc1622747e4418975a0
frc-mechmagicians <mindstormakash@gmail.com>
2024-03-10 16:56:23 -0700

    inverted gyro

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -63,7 +63,7 @@ public class DriveSubsystem extends SubsystemBase {
   SwerveDriveOdometry m_odometry =
       new SwerveDriveOdometry(
           DriveConstants.kDriveKinematics,
-          new Rotation2d(m_gyro.getAngle()*Math.PI/180),
+          new Rotation2d(-m_gyro.getAngle()*Math.PI/180),
           new SwerveModulePosition[] {
             m_frontLeft.getPosition(),
             m_frontRight.getPosition(),
@@ -79,7 +79,7 @@ public class DriveSubsystem extends SubsystemBase {
   public void periodic() {
     // Update the odometry in the periodic block
     m_odometry.update(
-        new Rotation2d(m_gyro.getAngle()*Math.PI/180),
+        new Rotation2d(-m_gyro.getAngle()*Math.PI/180),
         new SwerveModulePosition[] {
           m_frontLeft.getPosition(),
           m_frontRight.getPosition(),
@@ -105,7 +105,7 @@ public class DriveSubsystem extends SubsystemBase {
    */
   public void resetOdometry(Pose2d pose) {
     m_odometry.resetPosition(
-        new Rotation2d(m_gyro.getAngle()*Math.PI/180),
+        new Rotation2d(-m_gyro.getAngle()*Math.PI/180),
         new SwerveModulePosition[] {
           m_frontLeft.getPosition(),
           m_frontRight.getPosition(),
@@ -129,7 +129,7 @@ public class DriveSubsystem extends SubsystemBase {
             ChassisSpeeds.discretize(
                 fieldRelative
                     ? ChassisSpeeds.fromFieldRelativeSpeeds(
-                        xSpeed, ySpeed, rot, new Rotation2d(m_gyro.getAngle()*Math.PI/180))
+                        xSpeed, ySpeed, rot, new Rotation2d(-m_gyro.getAngle()*Math.PI/180))
                     : new ChassisSpeeds(xSpeed, ySpeed, rot),
                 DriveConstants.kDrivePeriod));
     SwerveDriveKinematics.desaturateWheelSpeeds(
@@ -186,7 +186,7 @@ public class DriveSubsystem extends SubsystemBase {
    * @return the robot's heading in degrees, from -180 to 180
    */
   public double getHeading() {
-    return m_gyro.getAngle();
+    return -m_gyro.getAngle();
   }
 
   /**
